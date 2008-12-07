aef6dd3fd84ad4644add9f3f64bc5a4f8b8cb7db
lara314 <lara314@b0e87d08-c45b-11dd-874b-8bacd04a0a74>
2008-12-07 12:46:31 +0000

    Added a folder remotely

---
+++ trunk/seg/main.cpp
@@ -0,0 +1,16 @@
+#include "Dictionary.h"
+#include "Sentence.h"
+#include "SentenceAnalyser.h"
+
+#include "tools/dictionary_sort.h"
+#include "./test/test_all.h"
+int main()
+{
+	/*sort_dictionary("C:\\large.txt", true);
+	sort_dictionary("C:\\large.txt", false);
+	test_sentence();
+	test_load_from_txt();
+	test_sentence_analyser();*/
+
+	test_all();
+}
