3c35fa9ce6e937ca5e780df2ae6b42c98cc6fff0
lara314 <lara314@b0e87d08-c45b-11dd-874b-8bacd04a0a74>
2008-12-07 12:47:59 +0000

    Added a folder remotely

---
+++ seg/tools/dictionary_sort.cpp
@@ -0,0 +1,188 @@
+#include "dictionary_sort.h"
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+// We assume that th
+const int CHARS_SIZE = 32;
+
+enum Result
+{
+	Smaller = -1,
+	Equal = 0,
+	Larger = 1
+};
+
+
+Result compare(const char* c1, const char* c2);
+int partitial(char** all_line, int begin, int end);
+void quick_sort(char** all_line, int begin, int end);
+void exchange( char* &a, char* &b);
+
+
+void sort_dictionary(const char * file_path, bool reserved)
+{
+	FILE* file = fopen(file_path, "r");
+	char buf[CHARS_SIZE];
+
+	int line = 0;
+	while(fgets(buf,CHARS_SIZE, file) != NULL)
+	{
+		// remove single words
+		if(strlen(buf) == 3) continue;
+		++line;
+	}
+	if( line == 0)  return;
+	
+	char** all_lines = new char*[line];
+	for(int i = 0; i < line; i++)
+	{
+		all_lines[i] = new char[CHARS_SIZE];
+	}
+
+	fseek(file,0,SEEK_SET);
+
+	int i = 0;
+	while(fgets(buf,CHARS_SIZE,file) != NULL)
+	{
+		if(strlen(buf) == 3) continue;
+		strcpy(all_lines[i++],buf);
+	}
+	fclose(file);
+
+	if(reserved)
+	{
+		for( int i = 0; i < line; ++i)
+		{
+			int len = ( strlen(all_lines[i])-1 ) / 2;
+			for(int j = 0; j < len/2; ++j)
+			{
+				char temp = all_lines[i][2*j];
+				all_lines[i][2*j] = all_lines[i][2*(len-j-1)];
+				all_lines[i][2*(len-j-1)] = temp;
+
+				temp = all_lines[i][2*j+1];
+				all_lines[i][2*j+1] = all_lines[i][2*(len-j-1)+1];
+				all_lines[i][2*(len-j-1)+1] = temp;
+			}
+		}
+	}
+
+	srand(time(0));
+	printf("\nBegin Quick Sort...");
+	quick_sort(all_lines, 0, line-1);
+	
+	char output_file[128];
+	char path[128];
+
+	if(reserved)
+	{
+		sprintf(output_file,"C:\\dict_reserved_%d.txt", time(0)); 
+	}
+	else
+	{
+		sprintf(output_file,"C:\\dict_%d.txt", time(0)); 
+	}
+	
+	FILE* output = fopen(output_file,"w");
+	
+	printf("\nSort Finish.Begin Write to File: %s",output_file);
+	
+	FILE * removed = fopen("C:\\removed.txt","w");
+	int j = 0;
+
+	fputs(all_lines[0], output);
+	for(int i=1; i < line; ++i)
+	{
+		if(compare(all_lines[i-1],all_lines[i]) == Result::Equal)
+		{
+			++j;
+			fputs(all_lines[i],removed);	
+			continue;
+		}
+		fputs(all_lines[i],output);
+	}
+	fclose(output);
+	printf("\nFinish.\n%d Lines Removed.\n", j);
+
+	for(int i = 0; i < line; ++i)
+	{
+		delete [] all_lines[i];
+	}
+	delete [] all_lines;
+}
+
+void exchange( char* &a, char* &b)
+{
+	char * temp = a;
+	a = b;
+	b = temp;
+}
+
+int partitial(char** all_line, int begin, int end)
+{
+	//printf("\nPartition %d -> %d", begin, end);
+	// random partitial
+	if(begin != end)
+	{
+		int r = (rand()<<3) % (end-begin) + begin;
+		exchange(all_line[r],all_line[end]);
+	//	printf("\tExchange %d <=> %d", r, end);
+	}
+	char *x = all_line[end];
+	int i = begin-1;
+	int j;
+	for( j = begin; j < end; ++j)
+	{
+		Result result = compare(all_line[j],x);
+		if( result == Result::Smaller || result == Result::Equal)
+		{
+			++i;
+			exchange(all_line[j],all_line[i]);
+		}
+	}
+	++i;
+	exchange(all_line[i],all_line[end]);
+
+//	printf("\tPartitial At: %d",i);
+	return i;
+}
+
+void quick_sort(char** all_line, int begin, int end)
+{
+	if( begin < end ) {
+		
+		int partition = partitial(all_line,begin, end);
+		
+		if( partition != begin) quick_sort(all_line,begin,partition-1);
+		if( partition != end) quick_sort(all_line,partition+1,end);
+	}
+}
+
+Result compare(const char* arr1, const char* arr2)
+{
+	int f = strlen(arr1);
+	int s = strlen(arr2);
+
+	int min = f<s ? f : s;
+	for(int i = 0; i < min; ++i)
+	{
+		unsigned char c1 = arr1[i];
+		unsigned char c2 = arr2[i];
+
+		if( c1 < c2) return Result::Smaller;
+		if ( c1 > c2) return Result::Larger;
+		
+		continue;
+	}
+
+	if( f == s) return Result::Equal;
+	if( f < s) return Result::Smaller;
+	if( f > s) return Result::Larger;
+}
+
+
+
---
+++ seg/tools/dictionary_sort.h
@@ -0,0 +1,9 @@
+
+#ifndef __DICTIONARY_SORT_H
+#define __DICTIONARY_SORT_H
+
+void sort_dictionary(const char * in_file, bool reserved=false);
+
+//quick sort
+
+#endif
